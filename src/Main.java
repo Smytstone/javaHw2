a9f1d2e6e422d8d0aeb7893d923846f7b63ee18b
Alex Shchelokov <alex171174@mail.ru>
2024-02-20 17:38:43 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 14125;
+        int rubForBonus = 20;
+
+        int ticketBonus = ticketCost / rubForBonus;
+        System.out.println(ticketBonus);
+    }
+}
